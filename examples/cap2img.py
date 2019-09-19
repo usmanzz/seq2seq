0b972510abe741f47ce02d3c39431b55ef3758be
usmanzz <40923749+usmanzz@users.noreply.github.com>
2019-09-19 16:27:56 +0500

    image to caption added

--- examples/cap2img.py
+++ examples/cap2img.py
@@ -18,7 +18,7 @@ class ImgTokenizer(SeqTokenizer):
         n = len(data[0])
         plt.figure(figsize=(n * 2, 4))
         for ind, item in enumerate(list(zip(*data))):
-            ax = plt.subplot(2, n, ind+1)
+            ax = plt.subplot(2, n, ind + 1)
             plt.imshow(item[0].reshape(self.img_shape))
             plt.gray()
             ax.get_xaxis().set_visible(False)
@@ -38,4 +38,12 @@ class Cap2Img(DatasetHandler):
     def __init__(self, english_sentences, img_sequences, img_shape):
         encoder_tokenizer = EngTokenizer(english_sentences, is_encoder=True)
         decoder_tokenizer = ImgTokenizer(img_shape, img_sequences)
+        super().__init__(encoder_tokenizer, decoder_tokenizer)
+
+
+class Img2Cap(DatasetHandler):
+
+    def __init__(self, english_sentences, img_sequences, img_shape):
+        encoder_tokenizer = ImgTokenizer(img_shape, img_sequences, is_encoder=True)
+        decoder_tokenizer = EngTokenizer(english_sentences)
         super().__init__(encoder_tokenizer, decoder_tokenizer)
