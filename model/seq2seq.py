18b3633467e3a73c0bba933a470b1afcd129eb87
usmanzz <40923749+usmanzz@users.noreply.github.com>
2019-09-27 15:46:01 +0500

    fixes

--- model/seq2seq.py
+++ model/seq2seq.py
@@ -1,5 +1,5 @@
 from __future__ import print_function
-from tensorflow.keras.layers import Input, Dense, TimeDistributed, Concatenate, LSTM, Embedding, Multiply
+from tensorflow.keras.layers import Input, Dense, TimeDistributed, Concatenate, LSTM, Embedding
 from tensorflow.keras.layers import Bidirectional, CuDNNLSTM
 from tensorflow.keras.models import Model
 import numpy as np
@@ -72,7 +72,8 @@ class Seq2seq(EncoderDecoder):
         decoder_state_input_c = Input(shape=(self.latent_dim,))
         embeddings = Embedding(self.data.num_decoder_tokens, self.embedding_dim)(decoder_inputs)
         decoder_outputs, state_h, state_c = self.decoder_layer(embeddings,
-                                                         initial_state=[decoder_state_input_h, decoder_state_input_c])
+                                                               initial_state=[decoder_state_input_h,
+                                                                              decoder_state_input_c])
         decoder_dense = Dense(self.data.num_decoder_tokens, activation='softmax')
         decoded_output = TimeDistributed(decoder_dense)(decoder_outputs)
         decoder_model = Model([decoder_inputs, decoder_state_input_h, decoder_state_input_c],
@@ -112,7 +113,8 @@ class Seq2seqAttention(EncoderDecoder):
         encoder_outputs = Input(shape=(self.data.max_encoder_seq_length, self.latent_dim))
         embeddings = Embedding(self.data.num_decoder_tokens, self.embedding_dim)(decoder_inputs)
         decoder_outputs, state_h, state_c = self.decoder_layer(embeddings,
-                                                         initial_state=[decoder_state_input_h, decoder_state_input_c])
+                                                               initial_state=[decoder_state_input_h,
+                                                                              decoder_state_input_c])
         attn_layer = AttentionLayer(name='attention_layer')
         # print(encoder_outputs.shape, decoder_outputs.shape)
         attn_out, attn_states = attn_layer([encoder_outputs, decoder_outputs])
@@ -141,14 +143,34 @@ class Seq2seqAttention(EncoderDecoder):
 class BiSeq2seqAttention(Seq2seqAttention):
 
     def __init__(self, latent_dim, data, embedding_dim=100, optimizer='adam'):
-        super().__init__(latent_dim, data, embedding_dim,optimizer=optimizer)
+        super().__init__(latent_dim, data, embedding_dim, optimizer=optimizer)
 
     def get_encoder(self):
         # Define an input sequence and process it.
         encoder_inputs = Input(shape=(self.data.max_encoder_seq_length,))
         embeddings = Embedding(self.data.num_encoder_tokens, self.embedding_dim)(encoder_inputs)
-        encoder_outputs, esh, esh1, esc, esc1 = Bidirectional(self.encoder_layer, merge_mode="mul")(embeddings)
-        encoder_model = Model(encoder_inputs, [encoder_outputs, Multiply()([esh, esh1]), Multiply()([esc, esc1])])
+        encoder_outputs, forward_h, forward_c, backward_h, backward_c = Bidirectional(self.encoder_layer)(embeddings)
+        encoder_model = Model(encoder_inputs, [encoder_outputs, Concatenate()([forward_h, backward_h]),
+                                               Concatenate()([forward_c, backward_c])])
         encoder_model.summary()
         return encoder_model
 
+    def get_decoder(self):
+        decoder_inputs = Input(shape=(None,))
+        decoder_state_input_h = Input(shape=(self.latent_dim * 2,))
+        decoder_state_input_c = Input(shape=(self.latent_dim * 2,))
+        encoder_outputs = Input(shape=(self.data.max_encoder_seq_length, self.latent_dim * 2))
+        embeddings = Embedding(self.data.num_decoder_tokens, self.embedding_dim)(decoder_inputs)
+        decoder_outputs, state_h, state_c = self.decoder_layer(embeddings,
+                                                               initial_state=[decoder_state_input_h,
+                                                                              decoder_state_input_c])
+        attn_layer = AttentionLayer(name='attention_layer')
+        # print(encoder_outputs.shape, decoder_outputs.shape)
+        attn_out, attn_states = attn_layer([encoder_outputs, decoder_outputs])
+        context_vectors = Concatenate()([attn_out, decoder_outputs])
+        decoder_dense = Dense(self.data.num_decoder_tokens, activation='softmax')
+        decoded_output = TimeDistributed(decoder_dense)(context_vectors)
+        decoder_model = Model([decoder_inputs, encoder_outputs, decoder_state_input_h, decoder_state_input_c],
+                              [decoded_output, state_h, state_c])
+        decoder_model.summary()
+        return decoder_model
