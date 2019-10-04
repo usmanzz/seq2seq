231d1c6928975da78167ad31bd3bb6da5fcd1a59
usmanzz <40923749+usmanzz@users.noreply.github.com>
2019-10-04 20:34:20 +0500

    fix

--- examples/nmt.py
+++ examples/nmt.py
@@ -1,7 +1,7 @@
 from ..utils.tokenizer import SeqTokenizer
 from ..utils.data import DatasetHandler
 import spacy, os
-from ..model.seq2seq import Seq2seq
+# from ..model.seq2seq import Seq2seq
 
 os.system('python -c "import thinc.neural.gpu_ops"')
 
@@ -47,6 +47,6 @@ if __name__ == '__main__':
     df = pd.read_csv("fra.txt", sep='\t', names=["eng", "fra"])
     df.tail()
     data = Fra2Eng(df["eng"].values, df["fra"].values)
-    seq2seq = Seq2seq(100, data)
-    seq2seq.train()
-    seq2seq.test()
+    # seq2seq = Seq2seq(100, data)
+    # seq2seq.train()
+    # seq2seq.test()
