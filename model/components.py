9db5bd987e3387278ea730e0612956f76d703c11
usmanzz <40923749+usmanzz@users.noreply.github.com>
2019-10-04 21:39:58 +0500

    try fix

--- model/components.py
+++ model/components.py
@@ -21,12 +21,12 @@ class BahdanauAttention(Model):
         # hidden shape == (batch_size, hidden size)
         # hidden_with_time_axis shape == (batch_size, 1, hidden size)
         # we are doing this to perform addition to calculate the score
-        hidden_with_time_axis = tf.expand_dims(query, 1)
+        # hidden_with_time_axis = tf.expand_dims(query, 1)
 
         # score shape == (batch_size, max_length, 1)
         # we get 1 at the last axis because we are applying score to self.V
         # the shape of the tensor before applying self.V is (batch_size, max_length, units)
-        score = self.V(tf.nn.tanh(self.W1(values) + self.W2(hidden_with_time_axis)))
+        score = self.V(tf.nn.tanh(self.W1(values) + self.W2(query)))
 
         # attention_weights shape == (batch_size, max_length, 1)
         attention_weights = tf.nn.softmax(score, axis=1)
